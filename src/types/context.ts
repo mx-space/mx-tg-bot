1b80f09556e00c4c53b5074b20c52cd71fd12c8b
Innei <i@innei.in>
2023-07-23 15:57:07 +0800

    chore: cleanup

--- src/types/context.ts
+++ src/types/context.ts
@@ -5,7 +5,6 @@ import {
   FastifyTypeProviderDefault,
   RawServerDefault,
 } from 'fastify'
-import TelegramBot from 'node-telegram-bot-api'
 import { Telegraf } from 'telegraf'
 
 export interface ModuleContext {
