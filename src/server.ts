1e98f1a439fdc989fb4c08adf29dc8365b467c0f
Innei <i@innei.in>
2023-10-26 17:12:29 +0800

    fix: ts happy
    Signed-off-by: Innei <i@innei.in>

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 // Require library to exit fastify process, gracefully (if possible)
-import closeWithGrace from 'close-with-grace'
 import * as dotenv from 'dotenv'
 // Require the framework
 import Fastify from 'fastify'
@@ -18,20 +17,6 @@ const app = Fastify({
 
 // Register your application as a normal plugin.
 void app.register(import('./app'))
-
-// Delay is the number of milliseconds for the graceful close to finish
-const closeListeners = closeWithGrace({ delay: 500 }, async (opts: any) => {
-  if (opts.err) {
-    app.log.error(opts.err)
-  }
-
-  await app.close()
-})
-
-app.addHook('onClose', async (_instance, done) => {
-  closeListeners.uninstall()
-  done()
-})
 
 // Start listening.
 void app.listen({
